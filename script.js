7a718b241da662ff1abd7e604eb93972d15d1002
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-04-30 11:38:20 +0800

    changes

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+/*
+movie_lst = []
+movie = input("Enter movie choices seperated by comma : ")
+movie_lst = movie.split(",")
+movie_choices = random.choice(movie_lst)
+print(f"Watch {movie_choices} today!")
+*/
