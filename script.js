1b8d136cb0b4e90335791d7c40a6c5439d0710ce
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-05-01 13:43:51 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-/*
-movie_lst = []
-movie = input("Enter movie choices seperated by comma : ")
-movie_lst = movie.split(",")
-movie_choices = random.choice(movie_lst)
-print(f"Watch {movie_choices} today!")
-*/
+let movie_lst = [];
+let movie = prompt("Enter movie choices separated by comma: ");
+movie_lst = movie.split(",");
+let movie_choices = movie_lst[Math.floor(Math.random() * movie_lst.length)];
+console.log(`Watch ${movie_choices} today!`);
